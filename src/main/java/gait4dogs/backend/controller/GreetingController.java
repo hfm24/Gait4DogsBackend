851fc286df3543c8a33403c483792697492aa170
Hayden McPhee <hfm24@drexel.edu>
2017-11-10 16:50:48 -0500

    basic REST service. Run and go to http://localhost:8080/greeting for example

---
+++ src/main/java/gait4dogs/backend/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package gait4dogs.backend.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import gait4dogs.backend.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
