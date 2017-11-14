80254ab3b6919fe831578a2229aa7eb134f92862
KFish244 <kjf54@drexel.edu>
2017-11-14 04:33:25 -0500

    Updated dog and session controller to include analytics

--- src/main/java/gait4dogs/backend/controller/DogController.java
+++ src/main/java/gait4dogs/backend/controller/DogController.java
@@ -3,6 +3,7 @@ package gait4dogs.backend.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import gait4dogs.backend.data.Dog;
+import gait4dogs.backend.data.DogAnalytics;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DogController {
 
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/dog/add")
@@ -21,5 +21,15 @@ public class DogController {
     @RequestMapping("/dog/get")
     public Dog getDog(@RequestParam(value="name", defaultValue = "Spot") String name){
         return new Dog("Spot", 5, 80, "German Shepherd", "October 8th, 2017", counter.incrementAndGet());
+    }
+
+    @RequestMapping("/dogAnalytics/add")
+    public DogAnalytics addDogAnalytics(){
+        return new DogAnalytics(counter.incrementAndGet());
+    }
+
+    @RequestMapping("/dogAnalytics/get")
+    public DogAnalytics getDogAnalytics(@RequestParam(value="id", defaultValue = "0") long id){
+        return new DogAnalytics(id);
     }
 }
--- src/main/java/gait4dogs/backend/controller/SessionController.java
+++ src/main/java/gait4dogs/backend/controller/SessionController.java
@@ -1,12 +1,17 @@
 package gait4dogs.backend.controller;
 
 import gait4dogs.backend.data.Session;
+import gait4dogs.backend.data.SessionAnalytics;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class SessionController {
+
+    private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/session/add")
     public Session addSession() {
@@ -16,5 +21,15 @@ public class SessionController {
     @RequestMapping("/session/get")
     public Session getSession(@RequestParam(value="id", defaultValue = "0") String id){
         return new Session(0, 5, "data", "This is a test");
+    }
+
+    @RequestMapping("/sessionAnalytics/add")
+    public SessionAnalytics addSessionAnalytics(){
+        return new SessionAnalytics(counter.incrementAndGet());
+    }
+
+    @RequestMapping("/sessionAnalytics/get")
+    public SessionAnalytics getSessionAnalytics(@RequestParam(value="id", defaultValue = "0") long id){
+        return new SessionAnalytics(id);
     }
 }
