d0b9c79ce4f7f41f0ce4e22c6564cda8b3ffb487
Hayden McPhee <hfm24@drexel.edu>
2017-12-01 16:37:01 -0500

    Built up more analytics

--- src/main/java/gait4dogs/backend/util/AnalysisUtil.java
+++ src/main/java/gait4dogs/backend/util/AnalysisUtil.java
@@ -1,4 +1,15 @@
 package gait4dogs.backend.util;
 
+import gait4dogs.backend.data.SessionAnalytics;
+import gait4dogs.backend.data.SessionRawData;
+
 public class AnalysisUtil {
+    public AnalysisUtil() {
+
+    }
+
+    public SessionAnalytics doSessionAnalysis(SessionRawData rawData, long sessionId) {
+
+        return new SessionAnalytics(sessionId);
+    }
 }
