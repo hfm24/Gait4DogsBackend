8cc270dd29f2c3555cc6f01ba824e2cfa62aca21
Hayden McPhee <hfm24@drexel.edu>
2018-05-14 17:26:40 -0400

    another quick fix

--- src/main/java/gait4dogs/backend/util/AnalysisUtil.java
+++ src/main/java/gait4dogs/backend/util/AnalysisUtil.java
@@ -350,7 +350,7 @@ public class AnalysisUtil {
 
             double angle = 180 - abs(x1 + x2);
             System.out.println(180 - abs(x1 + x2));
-            if(angle < 30 || angle > 200){
+            if(angle < 30 || angle > 360){
                 break;
             }
             angles.add(angle);
